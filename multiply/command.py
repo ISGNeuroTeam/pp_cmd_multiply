6cbb8ce46bea6449f8d94b2438066ffc10f61bb0
sromanov <sromanov@isgneuro.com>
2022-07-20 17:09:54 +0300

    add multiplication of numbers

--- multiply/command.py
+++ multiply/command.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from otlang.sdk.syntax import Positional, OTLType
 from pp_exec_env.base_command import BaseCommand, Syntax
@@ -14,8 +15,8 @@ class MultiplyCommand(BaseCommand):
 
     syntax = Syntax(
         [
-            Positional("first_column", required=True, otl_type=OTLType.TEXT),
-            Positional("second_column", required=True, otl_type=OTLType.TEXT),
+            Positional("first_multiplier", required=True, otl_type=OTLType.ALL),
+            Positional("second_multiplier", required=True, otl_type=OTLType.ALL),
         ],
     )
     use_timewindow = False  # Does not require time window arguments
@@ -24,25 +25,41 @@ class MultiplyCommand(BaseCommand):
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
         self.log_progress("Start multiply command")
         # that is how you get arguments
-        first_column = self.get_arg("first_column").value
-        second_column_argument = self.get_arg("second_column")
-        second_column = second_column_argument.value
-        result_column_name = second_column_argument.named_as
+        first_multiplier_argument = self.get_arg("first_multiplier")
+        if isinstance(first_multiplier_argument.value, str):
+            first_multiplier = df[first_multiplier_argument.value]
+        else:
+            first_multiplier = first_multiplier_argument.value
 
-        self.logger.debug(f"Command add get first positional argument = {first_column}")
+        second_multiplier_argument = self.get_arg("second_multiplier")
+        if isinstance(second_multiplier_argument.value, str):
+            second_multiplier = df[second_multiplier_argument.value]
+        else:
+            second_multiplier = second_multiplier_argument.value
+        result_column_name = second_multiplier_argument.named_as
+
+        if isinstance(first_multiplier, (int, float)) and isinstance(second_multiplier, (int, float)):
+            if result_column_name != "":
+                first_multiplier = np.array([first_multiplier] * df.shape[0])
+                second_multiplier = np.array([second_multiplier] * df.shape[0])
+            else:
+                first_multiplier = np.array([first_multiplier])
+                second_multiplier = np.array([second_multiplier])
+
+        self.logger.debug(f"Command add get first positional argument = {first_multiplier_argument.value}")
         self.logger.debug(
-            f"Command add get second positional argument = {second_column}"
+            f"Command add get second positional argument = {second_multiplier_argument.value}"
         )
 
         if result_column_name != "":
-            df[result_column_name] = df[first_column] * df[second_column]
+            df[result_column_name] = first_multiplier * second_multiplier
             self.logger.debug(f"New column name: {result_column_name}")
 
         else:
             df = pd.DataFrame(
                 {
-                    f"add_{first_column}_{second_column}": df[first_column].values
-                                                           * df[second_column].values
+                    f"multiply_{first_multiplier_argument.value}_{second_multiplier_argument.value}": first_multiplier * second_multiplier
+
                 }
             )
         self.log_progress("Multiplication is completed.", stage=1, total_stages=1)
